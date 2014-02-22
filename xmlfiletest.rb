1ad50f433f78dfb3f67dba26d9c55077cdca411c
Rob Church <robchur29@gmail.com>
2014-02-21 21:47:57 -0800

    refinements

--- xmlfiletest.rb
+++ xmlfiletest.rb
@@ -4,7 +4,7 @@ require 'SecureRandom'
 
 a = Time.now
 
-numRecordsDesired = 2000000 # number of eligibility requests desired
+numRecordsDesired = 100000 # number of eligibility requests desired
 numMonths = 12 # number of months to create eligibility requests for
 numYears = 3 # how many years of data to create
 numYearStart = 2014 # year in which you would like to start the eligibility records
@@ -14,7 +14,11 @@ xml = Builder::XmlMarkup.new(:target => f, :indent => 1)
 
 xml.tag!("tns:upass", "xmlns:tns".to_sym => "http://ns.translink.ca/upass/1.10-poc") do
 
-    (1..(numRecordsDesired/numMonths)/numYears).each do |numRecord|
+    numTimestoLoop = (numRecordsDesired/numMonths)/numYears
+    (1..numTimestoLoop).each do |numRecord|
+
+        numComplete = ((numRecord/numTimestoLoop)*100)
+        print "\r#{numComplete}% remaining  "
 
         myUUID = SecureRandom.uuid
 
@@ -34,9 +38,9 @@ xml.tag!("tns:upass", "xmlns:tns".to_sym => "http://ns.translink.ca/upass/1.10-p
 
         end # Years do
 
-    end # Records do aka the Student
+    end # Records do (aka the Student)
 
 end # tns:upass
 
 } #file open
-puts (Time.now-a).to_s
+puts "100% Complete - (" + (Time.now-a).to_s + ")"
